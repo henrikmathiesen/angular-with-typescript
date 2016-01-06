6710c51321ce22624adb69b07bc8c415310d1d6d
Henrik <henrik_mathiesen@hotmail.com>
2016-01-06 15:57:55 +0100

    -data access service

---
+++ app/shared/data-access.service.ts
@@ -0,0 +1,28 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+module main.shared {
+    
+    interface IDataAccessService {
+        getProductResource(): ng.resource.IResourceClass<IProductResource>;
+    }
+    
+    // We want an interface for the return type of $resource
+    interface IProductResource extends ng.resource.IResource<main.domain.IProduct> {
+        
+    }
+    
+    class DataAccessService implements IDataAccessService {
+        constructor(private $resource: ng.resource.IResourceService){
+            
+        }
+        
+        getProductResource(): ng.resource.IResourceClass<IProductResource> {
+            return this.$resource('/api/products/:productId');
+        }
+    } 
+    
+    angular
+        .module('main.shared')
+        .service('dataAccessService', DataAccessService)
+        
+}
---
+++ app/shared/shared.module.ts
@@ -0,0 +1,5 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+module main.shared {
+    angular.module('main.shared', ['ngResource']);
+}
