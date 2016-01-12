e01416a2c571cbf7cf10cfd8ec99d48b4eda40b1
Henrik Mathiesen <henrik_mathiesen@hotmail.com>
2016-01-12 17:36:48 +0100

    -added reference path to angular typing

--- app/shared/slugcheck.directive.ts
+++ app/shared/slugcheck.directive.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
 // From Pluralsight Course
 // https://app.pluralsight.com/library/courses/using-typescript-large-angularjs-apps/table-of-contents
 //
